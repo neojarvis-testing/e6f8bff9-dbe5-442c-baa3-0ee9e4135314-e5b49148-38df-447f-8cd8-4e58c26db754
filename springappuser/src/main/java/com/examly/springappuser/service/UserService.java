1ae7f3904917e6fdd5d0c7a9c90d430e3b8522db
ATCHAYAB <113198950+ATCHAYAB@users.noreply.github.com>
2025-05-28 14:57:15 +0530

    Updated UserService

--- springappuser/src/main/java/com/examly/springappuser/service/UserService.java
+++ springappuser/src/main/java/com/examly/springappuser/service/UserService.java
@@ -1,3 +1,9 @@
-public class UserService {
 
+package com.examly.springappuser.service;
+
+import com.examly.springappuser.model.User;
+
+public interface UserService {
+    User createUser(User user);
+    User loginUser(String email,String password);
 }
