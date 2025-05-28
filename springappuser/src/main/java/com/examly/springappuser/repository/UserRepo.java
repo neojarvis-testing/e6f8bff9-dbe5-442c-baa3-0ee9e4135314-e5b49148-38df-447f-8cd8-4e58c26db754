aac3ccdedd85490e8df157219e08f8468cc99afd
ATCHAYAB <113198950+ATCHAYAB@users.noreply.github.com>
2025-05-28 14:50:13 +0530

    Updated UserRepo

--- springappuser/src/main/java/com/examly/springappuser/repository/UserRepo.java
+++ springappuser/src/main/java/com/examly/springappuser/repository/UserRepo.java
@@ -1,5 +1,12 @@
-package main.java.com.examly.springappuser.repository;
+package com.examly.springappuser.repository;
 
-public interface UserRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-}
+import com.examly.springappuser.model.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,Long>{
+  User findByEmail(String email);
+  User findByUsername(String username);
+} 
