db7e71ff973ca481a88a8db6dfc51584040c2add
ATCHAYAB <113198950+ATCHAYAB@users.noreply.github.com>
2025-05-28 14:37:58 +0530

    Updated LoginResponse

--- springappuser/src/main/java/com/examly/springappuser/controller/LoginResponse.java
+++ springappuser/src/main/java/com/examly/springappuser/controller/LoginResponse.java
@@ -1,3 +1,11 @@
-public class LoginResponse {
 
-}
+package com.examly.springappuser.controller;
+
+public class LoginResponse {
+    private String token;
+    public LoginResponse(String token){
+        this.token=token;
+    }
+    public String getToken(){
+        return token;
+    }
