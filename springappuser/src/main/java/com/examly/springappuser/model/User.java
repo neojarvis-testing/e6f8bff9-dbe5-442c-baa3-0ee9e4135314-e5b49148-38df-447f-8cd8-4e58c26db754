3dc6acf19913a5a3d1d0606a9f79bdb6427577b8
ATCHAYAB <113198950+ATCHAYAB@users.noreply.github.com>
2025-05-28 14:41:44 +0530

    Updated User

--- springappuser/src/main/java/com/examly/springappuser/model/User.java
+++ springappuser/src/main/java/com/examly/springappuser/model/User.java
@@ -1,18 +1,67 @@
+
+
 package com.examly.springappuser.model;
 
-import java.lang.annotation.Inherited;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-@Table(name="users")
 public class User {
-    @Id private Long userId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
     private String email;
     private String password;
     private String username;
-    private String userRole;
     private String mobileNumber;
-
+    private String userRole;
+    
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+    
+   
+    @Override
+    public String toString() {
+        return "User [userId=" + userId + ", email=" + email + ", password=" + password + ", username=" + username
+                + ", mobileNumber=" + mobileNumber + ", userRole=" + userRole + "]";
+    }
+    public String getUserRole() {
+        return userRole;
+    }
+    public void setUserRole(String userRole) {
+              this.userRole = userRole;
+    }
+    
+    
     
 }
