3bc4c5618cb2e1b7801aaa77b7dc34840f71c9df
ATCHAYAB <113198950+ATCHAYAB@users.noreply.github.com>
2025-05-28 14:37:21 +0530

    Updated AuthController

--- springappuser/src/main/java/com/examly/springappuser/controller/AuthController.java
+++ springappuser/src/main/java/com/examly/springappuser/controller/AuthController.java
@@ -1,5 +1,49 @@
-package main.java.com.examly.springappuser.controller;
 
+package com.examly.springappuser.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.examly.springappuser.config.JwtUtils;
+import com.examly.springappuser.model.LoginDTO;
+import com.examly.springappuser.model.User;
+import com.examly.springappuser.service.UserService;
+
+@RestController
+@RequestMapping("/api")
 public class AuthController {
-
+    private UserService userService;
+    private JwtUtils jwtUtils;
+    @Autowired
+    public AuthController(UserService userService, JwtUtils jwtUtils) {
+        this.userService = userService;
+        this.jwtUtils = jwtUtils;
+    }
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user){
+        User savedUser = userService.createUser(user);
+        System.out.println(savedUser.getUserRole());
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginDTO request){
+        User user = userService.loginUser(request.getEmail(), request.getPassword());
+        String token = jwtUtils.generateToken(user.getEmail());
+        System.out.println("------------------------------------------------");
+        System.out.println("------------------------------------------------");
+        System.out.println("------------------------------------------------");
+        System.out.println("Token -----------------------"+token);
+        System.out.println("------------------------------------------------");
+        System.out.println("------------------------------------------------");
+        System.out.println("------------------------------------------------");
+        return ResponseEntity.ok(new LoginResponse(token));
+    }
+    
 }
+
