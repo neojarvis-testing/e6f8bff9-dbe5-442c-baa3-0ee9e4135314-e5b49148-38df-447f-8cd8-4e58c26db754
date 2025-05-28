02ff3ed3b79110feca28f27d51e0a05421ccb1c8
ATCHAYAB <113198950+ATCHAYAB@users.noreply.github.com>
2025-05-28 13:09:13 +0530

    Updated UserPrinciple

--- springappuser/src/main/java/com/examly/springappuser/config/UserPrinciple.java
+++ springappuser/src/main/java/com/examly/springappuser/config/UserPrinciple.java
@@ -1,3 +1,73 @@
-public class UserPrinciple {
+package com.examly.springappuser.config;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.examly.springappuser.model.User;
+
+public class UserPrinciple implements UserDetails {
+    private static final long serialVersionUID=1L;
+    private transient User user;
+    public UserPrinciple(User user){
+        this.user=Objects.requireNonNull(user,"User cannot be null");
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(new SimpleGrantedAuthority("ROLE_"+user.getUserRole()));
+    }
+    @Override
+    public String getPassword(){
+        return user.getPassword();
+    }
+    @Override
+    public String getUsername(){
+        return user.getEmail();
+    }
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UserPrinciple other = (UserPrinciple) obj;
+        if (user == null) {
+            if (other.user != null)
+                return false;
+        } else if (!user.equals(other.user))
+            return false;
+        return true;
+    }
+    
 }
+
+
