38adec5c8268843eced958e8dc445cfafae2f9b5
ATCHAYAB <113198950+ATCHAYAB@users.noreply.github.com>
2025-05-28 13:05:12 +0530

    Updated JwtUtils

--- springappuser/src/main/java/com/examly/springappuser/config/JwtUtils.java
+++ springappuser/src/main/java/com/examly/springappuser/config/JwtUtils.java
@@ -1,3 +1,45 @@
-public class JwtUtils {
 
-}
+package com.examly.springappuser.config;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtils {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    public String getJwtSecret() {
+        return jwtSecret;
+    }
+
+    public long getJwtExpiration() {
+        return jwtExpiration;
+    }
+    public String generateToken(String email){
+        return Jwts.builder().setSubject(email).setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis()+jwtExpiration))
+        .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+    }
+
+    public String extractUsername(String token){
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        }
+        catch(JwtException|IllegalArgumentException e){
+            return false;
+        }
+    }
+ }
+
