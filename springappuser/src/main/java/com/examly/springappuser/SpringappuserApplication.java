d6324fbae45a05e7730b94c0b8438c3477a53f5e
ATCHAYAB <113198950+ATCHAYAB@users.noreply.github.com>
2025-05-28 15:05:18 +0530

    Updated SpringappuserApplication

--- springappuser/src/main/java/com/examly/springappuser/SpringappuserApplication.java
+++ springappuser/src/main/java/com/examly/springappuser/SpringappuserApplication.java
@@ -1,9 +1,12 @@
+
 package com.examly.springappuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringappuserApplication {
 
 	public static void main(String[] args) {
