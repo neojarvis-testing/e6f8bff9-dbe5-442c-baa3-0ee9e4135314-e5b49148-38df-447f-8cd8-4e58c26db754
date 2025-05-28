3d0004e48ae79eb11cad86905a6f27bc6dc7d440
ATCHAYAB <113198950+ATCHAYAB@users.noreply.github.com>
2025-05-28 13:06:28 +0530

    Updated MyUserDetailsService

--- springappuser/src/main/java/com/examly/springappuser/config/MyUserDetailsService.java
+++ springappuser/src/main/java/com/examly/springappuser/config/MyUserDetailsService.java
@@ -1,3 +1,32 @@
-public class MyUserDetailsService {
+package com.examly.springappuser.config;
 
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.examly.springappuser.model.User;
+import com.examly.springappuser.repository.UserRepo;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService{
+    
+    private UserRepo userRepo;
+    
+    @Autowired
+    public MyUserDetailsService(UserRepo userRepo){
+        this.userRepo=userRepo;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        User user = userRepo.findByEmail(email);
+        if(user==null){
+            throw new UsernameNotFoundException("User not found with email: "+email);
+        }
+        return new UserPrinciple(user);
+    }
 }
+
