d7c098076a14940526b9b636d161e69c7616a0a3
ATCHAYAB <113198950+ATCHAYAB@users.noreply.github.com>
2025-05-29 07:40:09 +0530

    Updated Feedback

--- springappfeedback/src/main/java/com/examly/springappfeedback/model/Feedback.java
+++ springappfeedback/src/main/java/com/examly/springappfeedback/model/Feedback.java
@@ -1,3 +1,95 @@
+package com.examly.springappfeedback.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Feedback {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long feedbackId;
+
+    private String feedbackText;
+
+    private LocalDate date;
+
+    private String category;
+
+    private int rating;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "driverId", nullable = false)
+    @JsonProperty("driverscheme")
+    private DriverU driverU;
+
+    // Getters and Setters
+
+    public long getFeedbackId() {
+        return feedbackId;
+    }
+
+    public void setFeedbackId(long feedbackId) {
+        this.feedbackId = feedbackId;
+    }
+
+    public String getFeedbackText() {
+        return feedbackText;
+    }
+
+    public void setFeedbackText(String feedbackText) {
+        this.feedbackText = feedbackText;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public DriverU getDriverU() {
+        return driverU;
+    }
+
+    public void setDriverU(DriverU driverU) {
+        this.driverU = driverU;
+    }
 }
