4cd99450b0e9bc896e8062cd0ed26112ab69649a
ATCHAYAB <113198950+ATCHAYAB@users.noreply.github.com>
2025-05-29 07:54:17 +0530

    Updated UserRepo

--- springappfeedback/src/main/java/com/examly/springappfeedback/repository/UserRepo.java
+++ springappfeedback/src/main/java/com/examly/springappfeedback/repository/UserRepo.java
@@ -1,3 +1,10 @@
-public interface UserRepo {
+package com.examly.springappfeedback.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.examly.springappfeedback.model.User;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    
+    
 }
